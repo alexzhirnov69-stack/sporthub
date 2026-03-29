1b10334e0ad0ff5a364d18ec0da84fbc321c0884
alexzhirnov69-stack <alexzhirnov69@gmail.com>
2026-03-29 14:44:58 +0300

    Add Cloudinary image upload

---
+++ config/cloudinary.js
@@ -0,0 +1,10 @@
+﻿// Cloudinary configuration
+require('dotenv').config();
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    secure: true
+});
+
+module.exports = cloudinary;
