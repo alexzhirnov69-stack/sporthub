51f2596cd3b97e5a1a522df58d7880ad794a2e9a
alexzhirnov69-stack <alexzhirnov69@gmail.com>
2026-03-30 14:30:17 +0300

    Fix theme toggle

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
+    cloud_name: 'dqaw6rr9m',
+    api_key: '813618665743722',
+    api_secret: 'DrtWjchXCGRMVKbNv6q4GVwrUbk',
     secure: true
 });
 
